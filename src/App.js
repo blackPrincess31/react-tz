f5fe30897074264e3f0fe169b9dc15b1d5efcc1a
blackPrincess31 <katesavchenko31@gmail.com>
2022-10-29 10:33:31 +0300

    + cdn scrirts for bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import LogForm from "./Form";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js";
+import "https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js";
+import "https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js";
 
 function App() {
   return (
